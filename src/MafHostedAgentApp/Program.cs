9d596c292b51a1dce5325d319d128ea52c05c762
Yuta Matsumura <tkmbmbr@gmail.com>
2026-06-14 14:40:20 +0900

    impl

--- src/MafHostedAgentApp/Program.cs
+++ src/MafHostedAgentApp/Program.cs
@@ -1,19 +1,17 @@
 ﻿// https://learn.microsoft.com/ja-jp/agent-framework/hosting/foundry-hosted-agent?pivots=programming-language-csharp
 // https://github.com/microsoft-foundry/foundry-samples/tree/main/samples/csharp/hosted-agents/agent-framework/simple-agent
+using Azure.AI.AgentServer.Core;
 using Azure.AI.Projects;
 using Azure.Identity;
-
+using DotNetEnv;
 using Microsoft.Agents.AI;
 using Microsoft.Agents.AI.Foundry.Hosting;
-using Microsoft.Extensions.Configuration;
 
-var configuration = new ConfigurationBuilder()
-    .AddUserSecrets<Program>()
-    .Build();
+Env.TraversePath().Load();
 
-var projectEndpoint = new Uri(configuration["FOUNDRY_PROJECT_ENDPOINT"]
-    ?? throw new InvalidOperationException("FOUNDRY_PROJECT_ENDPOINT is not set."));
-var deployment = configuration["AZURE_AI_MODEL_DEPLOYMENT_NAME"] ?? "gpt-4o";
+var projectEndpoint = new Uri(Environment.GetEnvironmentVariable("FOUNDRY_PROJECT_ENDPOINT")
+    ?? throw new InvalidOperationException("FOUNDRY_PROJECT_ENDPOINT environment variable is not set."));
+var deployment = Environment.GetEnvironmentVariable("AZURE_AI_MODEL_DEPLOYMENT_NAME") ?? "gpt-4o";
 
 AIAgent agent = new AIProjectClient(projectEndpoint, new DefaultAzureCredential())
     .AsAIAgent(
