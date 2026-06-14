df2e4182c99a0e0dae91eaba2d290126572e20fb
Yuta Matsumura <tkmbmbr@gmail.com>
2026-06-14 21:15:45 +0900

    impl invocations

--- src/MafHostedAgentApp/Program.cs
+++ src/MafHostedAgentApp/Program.cs
@@ -1,9 +1,11 @@
 ﻿// https://learn.microsoft.com/ja-jp/agent-framework/hosting/foundry-hosted-agent?pivots=programming-language-csharp
 // https://github.com/microsoft-foundry/foundry-samples/tree/main/samples/csharp/hosted-agents/agent-framework/simple-agent
-using Azure.AI.AgentServer.Core;
+using Azure.AI.AgentServer.Invocations;
 using Azure.AI.Projects;
 using Azure.Identity;
+
 using DotNetEnv;
+
 using Microsoft.Agents.AI;
 using Microsoft.Agents.AI.Foundry.Hosting;
 
@@ -20,8 +22,17 @@ AIAgent agent = new AIProjectClient(projectEndpoint, new DefaultAzureCredential(
         name: "MafHostedAgentApp");
 
 var builder = AgentHost.CreateBuilder(args);
+
+// responses
 builder.Services.AddFoundryResponses(agent);
 builder.RegisterProtocol("responses", endpoints => endpoints.MapFoundryResponses());
+
+// invocations
+// see https://github.com/microsoft-foundry/foundry-samples/tree/main/samples/csharp/hosted-agents/agent-framework/invocations-echo-agent
+builder.Services.AddSingleton<EchoAIAgent>();
+builder.Services.AddInvocationsServer();
+builder.Services.AddScoped<InvocationHandler, EchoInvocationHandler>();
+builder.RegisterProtocol("invocations", endpoints => endpoints.MapInvocationsServer());
 
 var app = builder.Build();
 app.Run();
